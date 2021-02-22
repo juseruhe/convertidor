166752a24f46ed5d9c12b5dfef32d1b07aeea1ce
Sebastian <jsruiz7411@misena.edu.co>
2021-02-22 16:12:40 -0500

    Terminado Converitdor

--- src/app/components/convertidor/convertidor.component.ts
+++ src/app/components/convertidor/convertidor.component.ts
@@ -19,4 +19,70 @@ export class ConvertidorComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  convertir(){
+
+    switch(this.tengo){
+
+      case 'USD':
+
+      if(this.quiero === 'USD'){
+
+        this.total = this.cantidad;
+      }
+
+      if(this.quiero === 'EUR'){
+
+        this.total = this.cantidad * 0.82
+      }
+
+      if(this.quiero === 'LIBRA'){
+
+        this.total = this.cantidad * 0.71
+      }
+
+      break;
+
+      case 'EUR':
+
+        if(this.quiero === 'USD'){
+
+          this.total = this.cantidad*1.22;
+        }
+  
+        if(this.quiero === 'EUR'){
+  
+          this.total = this.cantidad
+        }
+  
+        if(this.quiero === 'LIBRA'){
+  
+          this.total = this.cantidad * 0.86
+        }
+      break;
+
+      case 'LIBRA':
+
+        if(this.quiero === 'USD'){
+
+          this.total = this.cantidad*1.41;
+        }
+  
+        if(this.quiero === 'EUR'){
+  
+          this.total = this.cantidad * 1.16
+        }
+  
+        if(this.quiero === 'LIBRA'){
+  
+          this.total = this.cantidad 
+        }
+      break;
+
+
+
+
+
+    }
+  }
 }
